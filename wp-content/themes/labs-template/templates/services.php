a3bdc62869fd3ba0790ef40be7c9db8c6c1e8579
Souhaila <so.ouakrim@gmail.com>
2019-04-15 16:59:18 +0200

    Ajout du fichier functions.php

---
+++ wp-content/themes/labs-template/templates/services.php
@@ -0,0 +1,19 @@
+<!-- Services section -->
+<div class="services-section spad">
+    <div class="container">
+      <div class="section-title dark">
+        <h2>Get in <span>the Lab</span> and see the services</h2>
+      </div>
+      <div class="row">
+        <!-- single service -->
+        
+        <?php get_template_part('templates/services', 'modal'); ?>
+
+      </div>
+      <div class="text-center">
+        <a href="" class="site-btn">Browse</a>
+      </div>
+    </div>
+  </div>
+  <!-- services section end -->
+
