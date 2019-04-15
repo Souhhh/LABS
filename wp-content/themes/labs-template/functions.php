649bec4ef0bd1b79ed50ba7575c2e9045307cb02
Souhaila <so.ouakrim@gmail.com>
2019-04-15 18:29:39 +0200

    Rendu titre dynamique

--- wp-content/themes/labs-template/functions.php
+++ wp-content/themes/labs-template/functions.php
@@ -17,6 +17,14 @@ function ajout_css_js()
     wp_enqueue_style('carousel', get_template_directory_uri() . '/css/owl.carousel.css');
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
 
+
+    // Ajout des scripts js
+    wp_enqueue_script('jquery-perso', get_template_directory_uri() . '/js/jquery-2.1.4.min.js', null, true);
+    wp_enqueue_script('jquery-perso', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery-perso'], null, true);
+    wp_enqueue_script('magnific-popup', get_template_directory_uri() . '/js/magnific-popup.min.js', ['jquery-perso'], null, true);
+    wp_enqueue_script('carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', ['jquery-perso'], null, true);
+    wp_enqueue_script('circle', get_template_directory_uri() . '/js/circle-progress.min.js', ['jquery-perso'], null, true);
+    wp_enqueue_script('main-js', get_template_directory_uri() . '/js/main.js', ['jquery-perso'], null, true);
 }
 
 // Nous ajoutons un écouteur d'événements pour nous prévenir lorsque l'on peut ajouter des css et scripts.
