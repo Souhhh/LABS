f434f2d5aeca9b5fcccdc77afd304d4ac07a1a5d
Souhaila <so.ouakrim@gmail.com>
2019-05-08 15:06:58 +0200

    Ajout de la pagination pour afficher tous les services de la page Services OK

--- wp-content/themes/labs-template/templates/services/services.php
+++ wp-content/themes/labs-template/templates/services/services.php
@@ -12,18 +12,25 @@ $title3 = get_theme_mod('labs-serv-title3');
       <h2><?= $title1; ?> <span><?= $title2; ?></span> <?= $title3; ?></h2>
     </div>
     <div class="row">
+
+      <?php
+      $paged = (get_query_var('paged')) ?
+      get_query_var('paged') : 1;
+      ?>
+
       <?php
       $args = [
         'post_type' => 'services',
-        'orderby' => 'ASC',
-        // 'posts_per_page' => 'all',
+        'order' => 'ASC',
+        'posts_per_page' => 9,
+        'paged' => $paged,
       ];
       $query = new WP_Query($args);
       ?>
 
       <?php while ($query->have_posts()) :
         $query->the_post();
-        $icon = get_post_meta(get_the_ID(), 'choix_icone', true);
+        $icon = get_post_meta(get_the_ID($query), 'choix_icone', true);
         ?>
 
         <!-- single service -->
@@ -51,10 +58,26 @@ $title3 = get_theme_mod('labs-serv-title3');
   </div>
 
       <!-- Pagination -->
-      <div class="page-pagination text-center">
+      <!-- <div class="page-pagination text-center">
         <a class="active" href="">01.</a>
         <a href="">02.</a>
         <a href="">03.</a>
+      </div> -->
+
+
+      <div class="page-pagination text-center">
+        <?php
+        $current_page = max(1, get_query_var('paged'));
+        echo paginate_links(array(
+          'format' => '/page/%#%',
+          'current' => $current_page,
+          'total' => $query->max_num_pages,
+          'prev_text' => __('« Prev'),
+          'next_text' => __('Next »'),
+        ));
+        wp_reset_postdata();
+        ?>
+
       </div>
 
 </div>
