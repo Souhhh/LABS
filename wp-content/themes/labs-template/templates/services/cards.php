d7fefc2c08a5c75b6245c961d81145e955c7b0e5
Souhaila <so.ouakrim@gmail.com>
2019-05-08 13:22:22 +0200

    PROJETS : pour la sous-section 'services, le bouton amène bien vers les derniers projets. Pour la sous-section 'projets', création de 2 boucles autour de l'image du gsm, récupération des images des 3 derniers projets ajoutés

--- wp-content/themes/labs-template/templates/services/cards.php
+++ wp-content/themes/labs-template/templates/services/cards.php
@@ -1,43 +1,34 @@
  <!-- services card section-->
- <div class="services-card-section spad">
+ <div class="services-card-section spad" id="cards">
     <div class="container">
       <div class="row">
+        <?php
+        $args = [
+          'post_type' => 'projets',
+          'posts_per_page' => 3,
+          'order' => 'DESC',
+        ];
+        $query = new WP_Query($args);
+        ?>
+        <?php while ($query->have_posts()) :
+        $query->the_post();
+        ?>
         <!-- Single Card -->
         <div class="col-md-4 col-sm-6">
           <div class="sv-card">
             <div class="card-img">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/card-1.jpg" alt="">
+              <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
             </div>
             <div class="card-text">
-              <h2>Get in the lab</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
+              <h2><?php the_title(); ?></h2>
+              <p><?php the_content(); ?></p>
             </div>
           </div>
         </div>
-        <!-- Single Card -->
-        <div class="col-md-4 col-sm-6">
-          <div class="sv-card">
-            <div class="card-img">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/card-2.jpg" alt="">
-            </div>
-            <div class="card-text">
-              <h2>Projects online</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-            </div>
-          </div>
-        </div>
-        <!-- Single Card -->
-        <div class="col-md-4 col-sm-12">
-          <div class="sv-card">
-            <div class="card-img">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/card-3.jpg" alt="">
-            </div>
-            <div class="card-text">
-              <h2>SMART MARKETING</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-            </div>
-          </div>
-        </div>
+      <?php
+      endwhile;
+      wp_reset_postdata();
+      ?>
       </div>
     </div>
   </div>
--- wp-content/themes/labs-template/templates/services/features.php
+++ wp-content/themes/labs-template/templates/services/features.php
@@ -5,87 +5,85 @@ $title3 = get_theme_mod('labs-proj-title3');
 ?>
 
 <!-- features section -->
-  <div class="team-section spad">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="section-title">
-        <h2><?= $title1; ?> <span><?= $title2; ?></span> <?= $title3; ?></h2>
+<div class="team-section spad">
+  <!-- <div class="overlay"></div> -->
+  <div class="container">
+    <div class="section-title">
+      <h2><?= $title1; ?> <span><?= $title2; ?></span> <?= $title3; ?></h2>
+    </div>
+    <div class="row">
+      <!-- feature item -->
+      <div class="col-md-4 col-sm-4 features">
+        <?php
+        $args = [
+          'post_type' => 'projets',
+          'orderby' => 'rand',
+          'posts_per_page' => 3,
+        ];
+        $query = new WP_Query($args);
+        ?>
+        <?php while ($query->have_posts()) :
+          $query->the_post();
+          $icon = get_post_meta(get_the_ID(), 'choix_icone', true);
+          ?>
+          <div class="icon-box light left">
+            <div class="service-text">
+              <h2><?php the_title(); ?></h2>
+              <p><?php the_content(); ?></p>
+            </div>
+            <div class="icon">
+              <i class="<?= $icon ?>"></i>
+            </div>
+          </div>
+        <?php
+      endwhile;
+      wp_reset_postdata();
+      ?>
       </div>
-      <div class="row">
-        <!-- feature item -->
-        <div class="col-md-4 col-sm-4 features">
-          <div class="icon-box light left">
-            <div class="service-text">
-              <h2>Get in the lab</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-            <div class="icon">
-              <i class="flaticon-002-caliper"></i>
-            </div>
-          </div>
-          <!-- feature item -->
-          <div class="icon-box light left">
-            <div class="service-text">
-              <h2>Projects online</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-            <div class="icon">
-              <i class="flaticon-019-coffee-cup"></i>
-            </div>
-          </div>
-          <!-- feature item -->
-          <div class="icon-box light left">
-            <div class="service-text">
-              <h2>SMART MARKETING</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-            <div class="icon">
-              <i class="flaticon-020-creativity"></i>
-            </div>
-          </div>
-        </div>
-        <!-- Devices -->
-        <div class="col-md-4 col-sm-4 devices">
-          <div class="text-center">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/device.png" alt="">
-          </div>
-        </div>
-        <!-- feature item -->
-        <div class="col-md-4 col-sm-4 features">
-          <div class="icon-box light">
-            <div class="icon">
-              <i class="flaticon-037-idea"></i>
-            </div>
-            <div class="service-text">
-              <h2>Get in the lab</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-          </div>
-          <!-- feature item -->
-          <div class="icon-box light">
-            <div class="icon">
-              <i class="flaticon-025-imagination"></i>
-            </div>
-            <div class="service-text">
-              <h2>Projects online</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-          </div>
-          <!-- feature item -->
-          <div class="icon-box light">
-            <div class="icon">
-              <i class="flaticon-008-team"></i>
-            </div>
-            <div class="service-text">
-              <h2>SMART MARKETING</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur ad ipiscing elit. Curabitur leo est, feugiat nec</p>
-            </div>
-          </div>
+
+
+      <!-- PHOTO DU GSM -->
+      <!-- Devices -->
+      <div class="col-md-4 col-sm-4 devices">
+        <div class="text-center">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/device.png" alt="">
         </div>
       </div>
-      <div class="text-center mt100">
-        <a href="" class="site-btn">Browse</a>
+
+
+      <!-- feature item -->
+      <div class="col-md-4 col-sm-4 features">
+        <?php
+        $args = [
+          'post_type' => 'projets',
+          'orderby' => 'rand',
+          'posts_per_page' => 3,
+        ];
+        $query = new WP_Query($args);
+        ?>
+        <?php while ($query->have_posts()) :
+          $query->the_post();
+          $icon = get_post_meta(get_the_ID(), 'choix_icone', true);
+          ?>
+          <div class="icon-box light">
+            <div class="icon">
+              <i class="<?= $icon ?>"></i>
+            </div>
+            <div class="service-text">
+              <h2><?php the_title(); ?></h2>
+              <p><?php the_content(); ?></p>
+            </div>
+          </div>
+        <?php
+      endwhile;
+      wp_reset_postdata();
+      ?>
       </div>
     </div>
   </div>
+
+  <div class="text-center mt100">
+    <a href="#cards" class="site-btn">Browse</a>
+  </div>
+</div>
   <!-- features section end-->
