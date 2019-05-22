4d9f5f5dc2295a1692bd995c02a318c84d78fdd9
Souhaila <so.ouakrim@gmail.com>
2019-05-22 12:12:49 +0200

    BLOG : j'ai changé la structure de la page quand on click sur un tag ou sur une catégorie. Avant, ça nous donnait une page blanche avec seulement le nom des articles qui ont le tag ou la catégorie. Maintenant, la page a la meme structure que celle de la page 'blog' et en plus j'ai fais en sorte de récupérer maintenant l'image de l'article et son titre (et pas uniquement le titre comme avant). Tous ces changement ont uniquement été fait dans le fichier archive.php

--- wp-content/themes/labs-template/archive.php
+++ wp-content/themes/labs-template/archive.php
@@ -1,27 +1,39 @@
 <?php
 get_header();
 // Ce fichier template spécial de WordPress est appelé pour afficher les catégories ou taxonomies ou autre archive. Voir le wp hierarchy.
+get_template_part('templates/blog/banner');
 ?>
 
+<div class="page-section spad">
+    <div class="container single-post-container">
+        <h1>
+            <?php the_archive_title(); ?>
+        </h1>
 
-<div class="container single-post-container">
-    <h1>
-        <?php the_archive_title(); ?>
-    </h1>
+        <div class="row">
+            <div class="col-sm-8 col-sm-7 blog-posts">
+                <ul class="list-group mb-5">
+                    <!-- Dans cette boucle, nous allons récupérer tous les posts qui correspondent à la recherche. -->
+                    <?php while (have_posts()) : the_post(); ?>
+                        <div class="single-post">
+                            <div class="post-thumbnail">
+                                <img src="<?php the_post_thumbnail_url(); ?>" alt=""><a href="<?php the_permalink(); ?>">
+                                    <h2 class="post-title">
+                                        <?php the_title(); ?>
+                                    </h2>
+                                </a>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </ul>
+            </div>
 
-    <ul class="list-group mb-5">
-        <!-- Dans cette boucle, nous allons récupérer tous les posts qui correspondent à la recherche. -->
-        <?php while (have_posts()) : the_post(); ?>
-            <li class="list-group-item">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?>
-                </a>
-            </li>
-        <?php endwhile; ?>
-    </ul>
+            <?php get_template_part('templates/blog/widget'); ?>
+        </div>
+    </div>
+
+    <?php
+    get_template_part('templates/services/newletter');
+    get_footer();
+    ?>
 </div>
-
-
-<?php
-get_footer();
-?>
